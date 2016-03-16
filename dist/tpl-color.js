466d6e2db74b169f901984c01ca863274d991f15
David Reiher <david@DR-iMac.local>
2016-03-16 15:34:44 +0100

    feat(rules): added global cssrule support

--- dist/tpl-color.js
+++ dist/tpl-color.js
@@ -4,49 +4,87 @@
 }());
 (function () {
   'use strict';
-  angular.module('tpl.color').service('tplColorService', TplColorService);
-  function TplColorService() {
-    // variables & constants
-    var COLOR_NAMES = [
-        'primaryColor',
-        'secondaryColor',
-        'primaryFontColor',
-        'secondaryFontColor',
-        'tertiaryColor'
-      ];
-    var colors = {};
-    // public functions export object
-    var exports = {
-        setColor: setColor,
-        getColors: getColors,
-        getColorNames: getColorNames
-      };
-    ////////////////////////////////////////////////////////////
-    //  PRIVATE FUNCTIONS
-    ////////////////////////////////////////////////////////////
-    function init() {
-      angular.forEach(COLOR_NAMES, function (name) {
-        colors[name] = null;
-      });
+  angular.module('tpl.color').service('tplColorService', [
+    '$document',
+    '$compile',
+    '$rootScope',
+    function ($document, $compile, $rootScope) {
+      // variables & constants
+      var COLOR_NAMES = [
+          'primaryColor',
+          'secondaryColor',
+          'primaryFontColor',
+          'secondaryFontColor',
+          'tertiaryColor'
+        ];
+      var colors = {};
+      var cssRules = {};
+      var cssElems = {};
+      // public functions export object
+      var exports = {
+          setColor: setColor,
+          getColors: getColors,
+          getColorNames: getColorNames,
+          addCssRule: addCssRule,
+          removeCssRule: removeCssRule
+        };
+      ////////////////////////////////////////////////////////////
+      //  PRIVATE FUNCTIONS
+      ////////////////////////////////////////////////////////////
+      function init() {
+        angular.forEach(COLOR_NAMES, function (name) {
+          colors[name] = null;
+        });
+      }
+      ////////////////////////////////////////////////////////////
+      //  PUBLIC FUNCTIONS
+      ////////////////////////////////////////////////////////////
+      function setColor(key, value) {
+        colors[key] = value;
+        angular.forEach(cssElems[value], function (elem) {
+          elem.remove();
+        });
+        angular.forEach(cssRules, function (rule, selector) {
+          angular.forEach(rule, function (color, property) {
+            if (color === key) {
+              var cssRule = '<style>' + selector + '{' + property + ':' + colors[color] + ';}</style>';
+              var elem = $compile(cssRule)($rootScope);
+              $document.find('head').append(elem);
+              if (!cssElems[color]) {
+                cssElems[color] = [];
+              }
+              cssElems[color].push(elem);  // WITHOUT MODIFIER
+            }
+          });
+        });
+      }
+      function getColors() {
+        return colors;
+      }
+      function getColorNames() {
+        return COLOR_NAMES;
+      }
+      function addCssRule(selector, property, color) {
+        if (!cssRules[selector]) {
+          cssRules[selector] = {};
+        }
+        cssRules[selector][property] = color;
+      }
+      function removeCssRule(selector, property) {
+        if (cssRules[selector] && cssRules[selector][property]) {
+          delete cssRules[selector][property];
+          if (Object.keys(cssRules[selector]) === 0) {
+            delete cssRules[selector];
+          }
+        }
+      }
+      ////////////////////////////////////////////////////////////
+      //  INITIALIZATION
+      ////////////////////////////////////////////////////////////
+      init();
+      return exports;
     }
-    ////////////////////////////////////////////////////////////
-    //  PUBLIC FUNCTIONS
-    ////////////////////////////////////////////////////////////
-    function setColor(key, value) {
-      colors[key] = value;
-    }
-    function getColors() {
-      return colors;
-    }
-    function getColorNames() {
-      return COLOR_NAMES;
-    }
-    ////////////////////////////////////////////////////////////
-    //  INITIALIZATION
-    ////////////////////////////////////////////////////////////
-    init();
-    return exports;
-  }
+  ]);
 }());
 (function () {
   'use strict';
--- src/tpl-color.service.js
+++ src/tpl-color.service.js
@@ -1,52 +1,101 @@
 (function() {
   'use strict';
+  angular.module('tpl.color').service('tplColorService', ['$document', '$compile', '$rootScope',
+    function($document, $compile, $rootScope) {
 
-  angular
-    .module('tpl.color')
-    .service('tplColorService', TplColorService);
+      // variables & constants
+      var COLOR_NAMES = [
+        'primaryColor',
+        'secondaryColor',
+        'primaryFontColor',
+        'secondaryFontColor',
+        'tertiaryColor'
+      ];
+      var colors = {};
+      var cssRules = {};
+      var cssElems = {};
 
-  function TplColorService() {
+      // public functions export object
+      var exports = {
+        setColor: setColor,
+        getColors: getColors,
+        getColorNames: getColorNames,
+        addCssRule: addCssRule,
+        removeCssRule: removeCssRule
+      };
 
-    // variables & constants
-    var COLOR_NAMES = ['primaryColor', 'secondaryColor', 'primaryFontColor', 'secondaryFontColor', 'tertiaryColor'];
-    var colors = {};
 
-    // public functions export object
-    var exports = {
-      setColor: setColor,
-      getColors: getColors,
-      getColorNames: getColorNames
-    };
+      ////////////////////////////////////////////////////////////
+      //  PRIVATE FUNCTIONS
+      ////////////////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////////////////
-    //  PRIVATE FUNCTIONS
-    ////////////////////////////////////////////////////////////
-    function init() {
-      angular.forEach(COLOR_NAMES, function(name) {
-        colors[name] = null;
-      });
+      function init() {
+        angular.forEach(COLOR_NAMES, function(name) {
+          colors[name] = null;
+        });
+      }
+
+
+      ////////////////////////////////////////////////////////////
+      //  PUBLIC FUNCTIONS
+      ////////////////////////////////////////////////////////////
+
+      function setColor(key, value) {
+        colors[key] = value;
+
+        angular.forEach(cssElems[value], function(elem) {
+          elem.remove();
+        });
+
+        angular.forEach(cssRules, function(rule, selector) {
+          angular.forEach(rule, function(color, property) {
+            if (color === key) {
+              var cssRule = '<style>' + selector + '{' + property + ':' + colors[color] + ';}</style>';
+              var elem = $compile(cssRule)($rootScope);
+
+              $document.find('head').append(elem);
+
+              if (!cssElems[color]) {
+                cssElems[color] = [];
+              }
+              cssElems[color].push(elem); // WITHOUT MODIFIER
+            }
+          });
+        });
+      }
+
+      function getColors() {
+        return colors;
+      }
+
+      function getColorNames() {
+        return COLOR_NAMES;
+      }
+
+      function addCssRule(selector, property, color) {
+        if (!cssRules[selector]) {
+          cssRules[selector] = {};
+        }
+        cssRules[selector][property] = color;
+      }
+
+      function removeCssRule(selector, property) {
+        if (cssRules[selector] && cssRules[selector][property]) {
+          delete cssRules[selector][property];
+          if (Object.keys(cssRules[selector]) === 0) {
+            delete cssRules[selector];
+          }
+        }
+      }
+
+      ////////////////////////////////////////////////////////////
+      //  INITIALIZATION
+      ////////////////////////////////////////////////////////////
+      init();
+
+
+
+      return exports;
     }
-
-    ////////////////////////////////////////////////////////////
-    //  PUBLIC FUNCTIONS
-    ////////////////////////////////////////////////////////////
-    function setColor(key, value) {
-      colors[key] = value;
-    }
-
-    function getColors() {
-      return colors;
-    }
-
-    function getColorNames() {
-      return COLOR_NAMES;
-    }
-
-    ////////////////////////////////////////////////////////////
-    //  INITIALIZATION
-    ////////////////////////////////////////////////////////////
-    init();
-    return exports;
-
-  }
-})();
+  ]);
+}());
